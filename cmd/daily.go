72fda5caf0a4f6065f3477dfb9c134ef9b52db57
Chetan Kini <chetan.d.kini@gmail.com>
2025-03-18 13:39:38 -0400

    add daily cmd

---
+++ cmd/daily.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"path/filepath"
+	"time"
+
+	"github.com/cdkini/oak/v2/helper"
+	"github.com/spf13/cobra"
+)
+
+var dailyCmd = &cobra.Command{
+	Use:   "daily",
+	Short: "Add a new daily note",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		oakRoot := cmd.Context().Value("root").(string)
+		return daily(oakRoot)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dailyCmd)
+}
+
+func daily(root string) error {
+	title := time.Now().Format("2006-01-02")
+	path := filepath.Join(root, title+".md")
+	return helper.OpenNote(path, title, []string{"daily"})
+}
