477fd83c6e19f021d863ca44860f53d8ae404c4e
Chetan Kini <chetan.d.kini@gmail.com>
2025-03-18 19:54:26 -0400

    add open cmd

--- cmd/open.go
+++ cmd/open.go
@@ -4,17 +4,32 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"github.com/cdkini/oak/v2/helper"
 	"github.com/spf13/cobra"
 )
 
 var openCmd = &cobra.Command{
 	Use:   "open <note(s)>",
 	Short: "Open note(s)",
+	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil // TODO
+		oakRoot := cmd.Context().Value("root").(string)
+
+		var query string
+		if len(args) == 0 {
+			query = ""
+		} else {
+			query = args[0]
+		}
+
+		return open(oakRoot, query)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(openCmd)
+}
+
+func open(root string, query string) error {
+	return helper.FZFOpen(root, query)
 }
--- helper/cli.go
+++ helper/cli.go
@@ -8,13 +8,14 @@ import (
 
 // TODO - Make these configurable
 const (
-	editor = "nvim"
-	grep   = "rg"
-	find   = "fd"
+	EDITOR = "nvim"
+	GREP   = "rg"
+	FIND   = "fd"
+	FZF    = "fzf"
 )
 
 func CheckDependencies() error {
-	for _, cmd := range []string{editor, grep, find} {
+	for _, cmd := range []string{EDITOR, GREP, FIND} {
 		if _, err := exec.LookPath(cmd); err != nil {
 			return err
 		}
@@ -45,23 +46,38 @@ func initNote(path string, title string, tags []string) error {
 }
 
 func openNoteInEditor(path string) error {
-	cmd := exec.Command(editor, path)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
+	return runCommand(EDITOR, nil, path)
 }
 
 func Grep(root string, query string) error {
-	cmd := exec.Command(grep, query)
-	cmd.Dir = root
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
+	return runCommand(GREP, &root, query)
 }
 
 func Find(root string, query string) error {
-	cmd := exec.Command(find, query)
-	cmd.Dir = root
+	return runCommand(FIND, &root, query)
+}
+
+func FZFOpen(root string, query string) error {
+	// TODO - remove hardcoded nvim, rg, and bat
+	reloadCmd := "reload:rg --column --color=always --smart-case {q} || :"
+	previewCmd := "bat --style=full --color=always --highlight-line {2} {1}"
+	args := []string{"--disabled", "--ansi", "--multi",
+		"--bind", "start:" + reloadCmd,
+		"--bind", "change:" + reloadCmd,
+		"--bind", "enter:become:if [[ $FZF_SELECT_COUNT -eq 0 ]]; then nvim {1} +{2}; else nvim +cw -q {+f}; fi",
+		"--delimiter", ":",
+		"--preview", previewCmd,
+		"--preview-window", "~4,+{2}+4/3,<80(up)",
+		"--query", query,
+	}
+	return runCommand(FZF, &root, args...)
+}
+
+func runCommand(name string, dir *string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	if dir != nil {
+		cmd.Dir = *dir
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	return cmd.Run()
